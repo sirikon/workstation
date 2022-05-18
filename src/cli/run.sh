2606ddc0cda5c3d346d2a9167ea590a75d30a0d8
Carlos Fdez. Llamas <hello@sirikon.me>
2022-05-18 10:38:42 +0200

    wip

--- src/cli/run.sh
+++ src/cli/run.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 set -euo pipefail
+
 ROOT="$(dirname "${BASH_SOURCE[0]}")"
-deno run \
+export SRK_ROOT=~/.srk
+export DENO_INSTALL="${SRK_ROOT}/deno/install"
+export DENO_DIR="${SRK_ROOT}/deno/cache"
+
+"${DENO_INSTALL}/bin/deno" run \
   -A --no-check \
   --config "${ROOT}/deno.json" \
   --import-map "${ROOT}/import_map.json" \
