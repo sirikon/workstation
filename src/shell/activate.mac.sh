6d3c16175c5de7b455d1ec7776a050cad306ad19
Carlos Fdez. Llamas <hello@sirikon.me>
2022-05-23 17:06:22 +0200

    sm for mac

--- src/shell/activate.mac.sh
+++ src/shell/activate.mac.sh
@@ -2,6 +2,10 @@
 
 export PATH=/opt/homebrew/bin:~/bin:$PATH
 
+function sm {
+  /Applications/Sublime\ Merge.app/Contents/SharedSupport/bin/smerge -n .
+}
+
 function backup-terminal-config { (
   cp ~/Library/Preferences/com.apple.Terminal.plist \
     ~/Dropbox/ProgramData/MacOsTerminal/com.apple.Terminal.plist
